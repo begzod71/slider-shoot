a3430cf18f90bb05d887d0b5205bd553273a595e
bekzod <bekzodabdunabiyev1371@gmail.com>
2023-01-16 17:22:53 +0500

    bold tamom

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const imgs = document.querySelector('#imgs')
+const leftBtn = document.querySelector('#left')
+const rightBtn = document.querySelector('#right')
+const img = document.querySelectorAll('#imgs img')
+
+let idx = 0;
+function changeImage(){
+    if (idx > img.length - 1) {
+        idx =0;
+    }else if(idx < 0){
+        idx = img.length - 1;
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+rightBtn.addEventListener('click' ,function change() {
+    idx++;
+    changeImage()
+});
+leftBtn.addEventListener('click' ,function change() {
+    idx--;
+    changeImage()
+});
+let interval = setInterval(run,2000);
+function run() {
+idx++;
+changeImage()  
+}
+function resetInterval() {
+    clearInterval(interval); 
+    interval = setInterval(run,2000);
+    
+}
